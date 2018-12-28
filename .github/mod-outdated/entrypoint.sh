2075c6ac9b51c531dbe7f160f5115c523c3d7a0c
Alexander Dorofeev <aka.spin@gmail.com>
2018-12-28 05:21:59 +0100

    remove side files from build tree

--- .github/mod-outdated/entrypoint.sh
+++ .github/mod-outdated/entrypoint.sh
@@ -5,10 +5,10 @@ set -ae
 GO111MODULE=on
 
 rm -f go.sum
-cp go.mod go.mod.bak
+cp go.mod /tmp/go.mod.bak
 go get -u
 go mod tidy
-mv go.mod go.mod.up
-mv go.mod.bak go.mod
-diff go.mod go.mod.up
+mv go.mod /tmp/go.mod.up
+mv /tmp/go.mod.bak go.mod
+diff go.mod /tmp/go.mod.up
 
